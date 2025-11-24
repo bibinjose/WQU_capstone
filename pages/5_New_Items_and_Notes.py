907fff70b23487cdd09d9eadc3f4e17460d3f00b
Bibin <bibinjose987@gmail.com>
2025-11-23 22:39:18 -0600

    Update 5_New_Items_and_Notes.py

--- pages/5_New_Items_and_Notes.py
+++ pages/5_New_Items_and_Notes.py
@@ -1,6 +1,8 @@
 import streamlit as st
 note = st.text_area("Notes for future devs:", key="dev_notes", height=300)
+st.caption(" Student Group 11203-Bibin Jose,Nicolas Vidal,Freddy Kuriakose")
 if st.button("Save notes"):
     with open("developer_notes.txt", "a", encoding="utf-8") as f:
         f.write(note + "\n" + "-"*50 + "\n")
     st.success("Notes saved locally!")
+
